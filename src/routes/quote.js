4b7aacf544ad58ce1441a6638073660aac591e7c
Anthon Holmqvist <anthon.kendel@gmail.com>
2017-03-25 17:50:14 +0100

    Add catch to promise

--- src/routes/quote.js
+++ src/routes/quote.js
@@ -9,8 +9,9 @@ faker.locale = "en";
 router.get("/get_random_quote/", function (req, res, next) {
     wiki.getRandomQuote().then(result => {
         res.send(quoteHelpers.createQuote(result));
+    }).catch(err => {
+        res.send(quoteHelpers.createQuote(err));
     });
-
 });
 
 router.get("/get_existing_quote/", function (req, res, next) {
