b21ce5f407adba1e9a3470fd9604ef0e0898142e
Erik Lilja <erik.liljaa@live.se>
2017-03-26 00:50:25 +0100

    Add margin

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 (function(){
 	let project = "Mean quote machine!"
+	let url='http://127.0.0.1:3000/get_random_quote'
 
 	var app = new Vue({
 		el: '#app',
@@ -26,7 +27,6 @@
 			},
 			retrieveQuote: function(cb) {
 				const context = this
-				let url='http://0.0.0.0:3000/get_random_quote'
 
 				// GET request
 				this.$http.get(url, function (data) {
