bd321cab67d0d39ffaedade49a4b1aed70c86382
Anthon Holmqvist <anthon.kendel@gmail.com>
2017-03-25 17:53:18 +0100

    Fix npm start

--- src/routes/quote.js
+++ src/routes/quote.js
@@ -1,10 +1,8 @@
-let faker = require("faker")
 let wiki = require('wikiquotesjs')
 let express = require("express");
 let router = express.Router();
 let quoteHelpers = require("../helpers/quoteHelpers");
 
-faker.locale = "en";
 
 router.get("/get_random_quote/", function (req, res, next) {
     wiki.getRandomQuote().then(result => {
