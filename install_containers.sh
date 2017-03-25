bd8102018f2f305b30874a27d1661ad4e85e2863
Max Meldrum <max@meldrum.se>
2017-03-25 18:01:41 +0100

    add variables for express and mongodb

--- install_containers.sh
+++ install_containers.sh
@@ -1,11 +1,21 @@
 #! /bin/sh
 
+# Simple script to get the environment up and running
+
+MONGODB_PORT=27017
+MONGODB_CONTAINER_NAME="mean-mongo"
+MONGODB_IMAGE_NAME="mongo"
+
+EXPRESS_PORT=3000
+EXPRESS_CONTAINER_NAME="mean-express"
+EXPRESS_IMAGE_NAME="mean-rest"
+
 # Mongo
-docker run -p 27017:27017 --name mean-mongo -d mongo 
+docker run -d --name $MONGODB_CONTAINER_NAME -p $MONGODB_PORT:$MONGODB_PORT $MONGODB_IMAGE_NAME
 
 # Express
-docker build -t mean-rest .
-docker run -d --name mean-express -p 3000:3000 mean-rest
+docker build -t $EXPRESS_IMAGE_NAME .
+docker run -d --name $EXPRESS_CONTAINER_NAME -p $EXPRESS_PORT:$EXPRESS_PORT $EXPRESS_IMAGE_NAME
 
 
 
