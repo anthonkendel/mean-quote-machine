567929ed94fff0f68bef1a1cda0f665bc78c9181
Anthon Holmqvist <anthon.kendel@gmail.com>
2017-03-25 20:00:43 +0100

    Add function for inserting quote to database

--- src/MongoDB.module.js
+++ src/MongoDB.module.js
@@ -6,8 +6,26 @@ let url = 'mongodb://localhost:27017/mean-mongo';
 
 // Use connect method to connect to the Server
 
-mongo.connect(url, function(err, db) {
+mongo.connect(url, function (err, db) {
     assert.equal(null, err);
-    console.log("Connected correctly to server");
+    console.log("Connected to mongodb");
     db.close();
 });
+
+module.exports = {
+    insertQuote: function (quote) {
+        mongo.connect(url, function (err, db) {
+            if (!assert.equal(null, err)) {
+                console.log("Connected correctly to server");
+                let quotes = db.collection("quotes");
+
+                quotes.insertOne(quote, function (err, result) {
+                    assert.equal(err, null);
+
+                    console.log(result.ops);
+                });
+            }
+            db.close();
+        });
+    }
+}
--- src/routes/Quote.module.js
+++ src/routes/Quote.module.js
@@ -3,9 +3,13 @@ let express = require("express");
 let router = express.Router();
 let quoteHelpers = require("../helpers/QuoteHelpers.module");
 
+let mongoDB = require("../MongoDB.module");
+
 
 router.get("/get_random_quote/", function (req, res, next) {
     wiki.getRandomQuote().then(result => {
+        mongoDB.insertQuote(result);
+
         res.send(quoteHelpers.formatQuote(result));
     }).catch(err => {
         res.send(quoteHelpers.formatQuote(err));
