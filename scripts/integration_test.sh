3cf33f78627a59ece11e2de97903fa64f735d397
Max Meldrum <max@meldrum.se>
2017-04-21 20:59:28 +0200

    Update integration test

--- scripts/integration_test.sh
+++ scripts/integration_test.sh
@@ -14,11 +14,12 @@ fi
 
 q2=$(curl -s $IP:$PORT/get_random_quote | jq -r '.quote.author')
 sleep 1
-test -n "$q2" || echo "Failed: Could not get random quote"; exit 1;
+test -n "$q2" || { echo "Failed: Could not get random quote"; exit 1; }
 
 # To ensure we can fetch that saved quote
-sleep 2
+sleep 3
 
 q3=$(curl -s $IP:$PORT/get_existing_quote | jq -r '.quote.author')
-test -n "$q3" || echo "Failed: Could not get existing quote after get_random_quoute should have inputted one"; exit 1;
+test -n "$q3" || { echo "Failed: Could not get existing quote after get_random_quoute should have inputted one"; exit 1; }
+
 
